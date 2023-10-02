bef2869f786417d4aa716826a141c276c02c64f9
Cotto213 <alfonsocotto5b12@gmail.com>
2023-10-02 11:22:41 -0600

    Project Setup

---
+++ piezas-vehiculo/App.js
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import {  Text, View } from 'react-native';
+
+
+import { NativeWindStyleSheet } from "nativewind";
+
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
+
+export default function App() {
+  return (
+    <View className="flex-1  items-center justify-center bg-gray-800">
+    <Text className="font-semibold text-white">Open up App.js to start working on your app!</Text>
+    <StatusBar style="auto" />
+  </View>
+  );
+}
+;
